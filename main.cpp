f5bff586b01d942debbf7c600324b24606eda31d
Zahra <133800367+ZeeBee287@users.noreply.github.com>
2025-03-13 21:17:06 +0500

    C++ Source File

--- main.cpp
+++ main.cpp
@@ -1,87 +1,94 @@
 #include <iostream>
 
 using namespace std;
+
 float dis(int, float);
+
 int main()
 {
-    double a;
+    int a; 
     float percentage;
     int option;
-    int num=0;
+    int num = 0;
     int price;
-    int sum=0;
-    float discount;
-    cout<<"Thank you for shopping at J&Z's Clothing Line"<<endl;
-    cout<<endl;
-    cout<<"Being human is given, but keeping our humanity is a choice"<<endl;
-    cout<<endl;
-    cout<<"J&Z's are proud to present amazing jaw dropping discounts for the international charity week from 25th November to 9th December"<<endl;
-    cout<<endl;
-    cout<<"Please enter the date when you shopped at J&Z's"<<endl;
-    tryagain:
-    cin>>a;
-    cout<<endl;
-    if(25<=a && a<=30)
-    {
-    cout<<"You have received a discount of 50%"<<endl;
-    percentage=50.0/100.0;
-     }
-    else if(1<=a && a<=5)
-    {
-    cout<<"You have received a discount of 30%"<<endl;
-    percentage=30.0/100.0;
-         }
-    else if(6<=a && a<=9)
-    {
-    cout<<"You have received a discount of 10%"<<endl;
-    percentage=10.0/100.0;
+    int sum = 0;
+    float discounted_price;
+
+    cout << "Thank you for shopping at J&Z's Clothing Line" << endl;
+    cout << "\nBeing human is given, but keeping our humanity is a choice\n" << endl;
+    cout << "J&Z's are proud to present amazing jaw-dropping discounts for the international charity week from 25th November to 9th December\n" << endl;
+
+    // Input validation for date
+    while (true) {
+        cout << "Please enter the date when you shopped at J&Z's (between 25 and 9): ";
+        cin >> a;
+
+        if (a >= 25 && a <= 30) {
+            cout << "You have received a discount of 50%" << endl;
+            percentage = 50.0 / 100.0;
+            break;
+        } 
+        else if (a >= 1 && a <= 5) {
+            cout << "You have received a discount of 30%" << endl;
+            percentage = 30.0 / 100.0;
+            break;
+        } 
+        else if (a >= 6 && a <= 9) {
+            cout << "You have received a discount of 10%" << endl;
+            percentage = 10.0 / 100.0;
+            break;
+        } 
+        else {
+            cout << "Invalid date! Please enter again." << endl;
+        }
     }
-    else
-    {
-    cout<<"You did not enter the correct date. Please enter the date again"<<endl;
-    goto tryagain;
+
+    // Get number of items and their prices
+    cout << "\nPlease enter the number of items you purchased: ";
+    cin >> num;
+
+    cout << "\nEnter the price of each item: " << endl;
+    for (int i = 0; i < num; i++) {
+        cin >> price;
+        sum += price;
     }
-    cout<<endl;
-    cout<<"Please enter the number of items you purchased"<<endl;
-    cin>>num;
-    cout<<endl;
-    cout<<"Please enter the price of the items (Prices are tagged on every item. If not, ask a worker to help you)"<<endl;
-    for(int i=0; i<num; i++)
-    {
-        cin>>price;
-        sum=sum+price;
+
+    cout << "\nYour total bill is: " << sum << endl;
+    
+    // Apply discount
+    discounted_price = dis(sum, percentage);
+    cout << "New bill after discount: " << discounted_price << endl;
+
+    // Charity selection
+    cout << "\nSelect an option for the charity your money will be donated to:\n";
+    cout << "1 - The Edhi Foundation\n";
+    cout << "2 - The Ansar Burney Trust\n";
+    cout << "3 - Shaukat Khanum Cancer Hospital\n";
+    cout << "Enter your choice: ";
+    cin >> option;
+
+    switch (option) {
+        case 1:
+            cout << "Thank you for donating to The Edhi Foundation." << endl;
+            break;
+        case 2:
+            cout << "Thank you for donating to The Ansar Burney Trust." << endl;
+            break;
+        case 3:
+            cout << "Thank you for donating to Shaukat Khanum Cancer Hospital." << endl;
+            break;
+        default:
+            cout << "Invalid selection!" << endl;
+            break;
     }
-    cout<<endl;
-    cout<<"Your total bill is "<<sum<<endl;
-    discount =dis(sum, percentage);
-    cout<<endl;
-    cout<<"new bill after discount is "<<discount<<endl;
-    cout<<endl;
-    cout<<"Select option for the charity your money will be donated to:"<<endl;
-    cout<<endl;
-    cout<<"Please press 1 to donate to The Edhi Foundation"<<endl;
-    cout<<"Please press 2 to donate to The Ansar Burney Trust"<<endl;
-    cout<<"Please press 3 to donate to Shaukat Khanum cancer hospital"<<endl;
-    cout<<endl;
-    cin>>option;
-    cout<<endl;
-    if(option==1)
-    cout<<"Thank you for donating to The Edhi foundation"<<endl;
-    else if(option==2)
-    cout<<"Thank you for donating to The Ansar Burney Trust"<<endl;
-    else if(option==3)
-    cout<<"Thank you for donating to Shaukat Khanum cancer hospital"<<endl;
-    else
-    cout<<"You did not select the correct option"<<endl;
-    cout<<endl;
-    cout<<"THANK YOU FOR SHOPPING AT J&Z'S. DO COME AGAIN"<<endl;
-    cout<<endl;
-    cout<<"Coded by Zahra Batool and Javeria Liaqat"<<endl;
+
+    cout << "\nTHANK YOU FOR SHOPPING AT J&Z'S. DO COME AGAIN!" << endl;
+    cout << "Coded by Zahra Batool and Javeria Liaqat" << endl;
+
     return 0;
 }
 
 float dis(int x, float y)
-{ float answer;
-  answer= x * y;
-  return answer;
+{
+    return x - (x * y);  // Returns final amount after discount
 }
